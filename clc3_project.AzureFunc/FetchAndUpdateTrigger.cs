3af451fe03c9cb8106c16b81a37a1b9bc45b525f
S2010454017 <s2010454017@students.fh-hagenberg.at>
2022-01-26 21:37:50 +0100

    add comment

--- clc3_project.AzureFunc/FetchAndUpdateTrigger.cs
+++ clc3_project.AzureFunc/FetchAndUpdateTrigger.cs
@@ -12,30 +12,43 @@ using clc3_project.AzureFunc;
 
 namespace FetchDataFunction
 {
+    /// <summary>
+    /// Class containing the logic for the Azure Function
+    /// </summary>
     public class FetchAndUpdateTrigger
     {
         private readonly HttpClient _client;
+        //URL to get information for new book
         private readonly string URL = "https://openlibrary.org/api/books?bibkeys=ISBN:{0}&jscmd=data&format=json";
         public FetchAndUpdateTrigger(IHttpClientFactory clientFactory)
         {
             _client = clientFactory.CreateClient();
         }
 
+        /// <summary>
+        /// The run method of the azure function. Can be triggered via the HTTP-Get method
+        /// </summary>
+        /// <param name="req">the httprequest</param>
+        /// <param name="log">Logger</param>
+        /// <returns>The book if data was obtained</returns>
         [FunctionName("FetchAndUpdateTrigger")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            // GET ISBN FROM ROUTE OR WHATEVER
+            //Get isbn from query
             string id = req.Query["isbn"];
             log.LogInformation($"ISBN {id} wanted...");
 
+            //send get request to external side
             var resp = await _client.GetAsync(string.Format(URL, id));
             if (resp.IsSuccessStatusCode)
             {
+                //read obtained data to string
                 var json = await resp.Content.ReadAsStringAsync();
                 if (!(string.IsNullOrEmpty(json) && json.Length > 2))
                 {
+                    //format obtained json to easier process it.
                     var obj = $"{{\"ISBN\":\"{id}\",";
                     var tmp = json.Remove(0, obj.Length);
                     obj += tmp.Remove(tmp.Length - 1, 1);
@@ -55,7 +68,7 @@ namespace FetchDataFunction
                         cat.Add((string)item.name);
                     }
 
-
+                    //create new book with obtained information and send it back to the caller.
                     var b = new Book { Authors = authors, Category = cat, ISBN = newBook.ISBN, BookName = newBook.title, 
                                        Cover = newBook.cover["large"] };
                     return new OkObjectResult(b);
